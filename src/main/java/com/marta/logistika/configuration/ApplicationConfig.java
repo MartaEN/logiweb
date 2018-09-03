9bac390a324b4c21fdeda4c6718dd5c634d543b5
MartaEN <marta.the.developer@gmail.com>
2018-09-03 11:11:52 +0300

    Basic configuration updated persistence.xml replaced with Java code configuration EntityManagerFactory configuration problem fixed

--- src/main/java/com/marta/logistika/configuration/ApplicationConfig.java
+++ src/main/java/com/marta/logistika/configuration/ApplicationConfig.java
@@ -2,9 +2,13 @@ package com.marta.logistika.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan("com.marta.logistika")
+@Import(value = {
+        DataSourceConfig.class,
+} )
 public class ApplicationConfig {
 
 
---
+++ src/main/java/com/marta/logistika/configuration/DataSourceConfig.java
@@ -0,0 +1,59 @@
+package com.marta.logistika.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource({ "classpath:datasource.properties" })
+public class DataSourceConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean("dataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("datasource.driver"));
+        dataSource.setUrl(env.getRequiredProperty("datasource.url"));
+        dataSource.setUsername(env.getRequiredProperty("datasource.user"));
+        dataSource.setPassword(env.getRequiredProperty("datasource.password"));
+        return dataSource;
+    }
+
+    @Bean("entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            @Qualifier("dataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        factoryBean.setPackagesToScan("com.marta.logistika.entity");
+        Properties properties = new Properties();
+        properties.put("hibernate.max_fetch_depth", env.getRequiredProperty("hibernate.max_fetch_depth"));
+        properties.put("hibernate.jdbc.fetch_size", env.getRequiredProperty("hibernate.jdbc.fetch_size"));
+        properties.put("hibernate.jdbc.batch_size", env.getRequiredProperty("hibernate.jdbc.batch_size"));
+        properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        factoryBean.setJpaProperties(properties);
+        return factoryBean;
+    }
+
+    @Bean("transactionManager")
+    public JpaTransactionManager transactionManager(@Qualifier("entityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
+        return transactionManager;
+    }
+}
