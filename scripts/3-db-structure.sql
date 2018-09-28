09d7aef2618077976a13387d44268ef3841dac24
MartaEN <marta.the.developer@gmail.com>
2018-09-28 11:21:12 +0300

    Test data updated

--- scripts/3-db-structure.sql
+++ scripts/3-db-structure.sql
@@ -7,46 +7,68 @@ USE `logistika`;
 -- ------------------------------------------------------
 -- Server version	5.7.21-log
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 --
 -- Table structure for table `cities`
 --
 
 DROP TABLE IF EXISTS `cities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cities` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_l61tawv0e2a93es77jkyvi7qa` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `drivers`
 --
 
 DROP TABLE IF EXISTS `drivers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `drivers` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `bookedUntil` datetime(6) NOT NULL,
   `firstName` varchar(65) NOT NULL,
   `lastName` varchar(65) NOT NULL,
   `personalId` varchar(12) NOT NULL,
+  `username` varchar(50) NOT NULL,
   `location` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_gq374lh46a7wq07rhyiuwt27m` (`personalId`),
+  UNIQUE KEY `UK_hfm3gk84eakexruf4mnv2tdpm` (`username`),
   KEY `FK5yhcy90whkuux8j8h8r07gsfc` (`location`),
   CONSTRAINT `FK5yhcy90whkuux8j8h8r07gsfc` FOREIGN KEY (`location`) REFERENCES `cities` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `orders`
 --
 
 DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `orders` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `creationDate` datetime(6) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `status` varchar(255) DEFAULT NULL,
+  `status` varchar(24) DEFAULT NULL,
   `weight` int(11) NOT NULL,
   `fromCity` bigint(20) NOT NULL,
   `toCity` bigint(20) NOT NULL,
@@ -55,7 +77,8 @@ CREATE TABLE `orders` (
   KEY `FKqtovmi7c9fcr4rahgoq7guaml` (`toCity`),
   CONSTRAINT `FKcbv7g0mx53fqfb0e318erdcfb` FOREIGN KEY (`fromCity`) REFERENCES `cities` (`id`),
   CONSTRAINT `FKqtovmi7c9fcr4rahgoq7guaml` FOREIGN KEY (`toCity`) REFERENCES `cities` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
@@ -63,6 +86,8 @@ CREATE TABLE `orders` (
 --
 
 DROP TABLE IF EXISTS `roads`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `roads` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `distance` int(11) NOT NULL,
@@ -74,6 +99,7 @@ CREATE TABLE `roads` (
   CONSTRAINT `FKgn48sywc7s7slaxs7m7xnyel9` FOREIGN KEY (`toCity`) REFERENCES `cities` (`id`),
   CONSTRAINT `FKhh9vlehh5r2ccb7bkhmj7uvgd` FOREIGN KEY (`fromCity`) REFERENCES `cities` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
@@ -81,6 +107,8 @@ CREATE TABLE `roads` (
 --
 
 DROP TABLE IF EXISTS `shifts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `shifts` (
   `trip` bigint(20) NOT NULL,
   `driver` bigint(20) NOT NULL,
@@ -89,12 +117,16 @@ CREATE TABLE `shifts` (
   CONSTRAINT `FK1nelyr4dj34suwxmf6k58k51b` FOREIGN KEY (`driver`) REFERENCES `drivers` (`id`),
   CONSTRAINT `FKby7d377ajtxfcakb3y5scdua0` FOREIGN KEY (`trip`) REFERENCES `tickets` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `stopovers`
 --
 
 DROP TABLE IF EXISTS `stopovers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `stopovers` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `duration` bigint(20) DEFAULT NULL,
@@ -107,7 +139,8 @@ CREATE TABLE `stopovers` (
   KEY `FKpfp4fpgk15f22k6s4greeolr8` (`trip`),
   CONSTRAINT `FK2nhhrxsjx1iiywu72g6rmdydu` FOREIGN KEY (`city`) REFERENCES `cities` (`id`),
   CONSTRAINT `FKpfp4fpgk15f22k6s4greeolr8` FOREIGN KEY (`trip`) REFERENCES `tickets` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
@@ -115,16 +148,21 @@ CREATE TABLE `stopovers` (
 --
 
 DROP TABLE IF EXISTS `tickets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tickets` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `arrival` datetime(6) DEFAULT NULL,
+  `avgLoad` int(11) NOT NULL,
+  `step` int(11) DEFAULT NULL,
   `departure` datetime(6) NOT NULL,
-  `status` varchar(255) DEFAULT NULL,
+  `status` varchar(24) DEFAULT NULL,
   `truck` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK3mo9x179xoplvp0tq0y920d8r` (`truck`),
   CONSTRAINT `FK3mo9x179xoplvp0tq0y920d8r` FOREIGN KEY (`truck`) REFERENCES `trucks` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
@@ -132,23 +170,29 @@ CREATE TABLE `tickets` (
 --
 
 DROP TABLE IF EXISTS `time_tracker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `time_tracker` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `finish` datetime(6) DEFAULT NULL,
   `minutes` bigint(20) DEFAULT NULL,
   `start` datetime(6) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
+  `status` varchar(24) DEFAULT NULL,
   `driver` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK5qsbxtn27vian263fx7awoayw` (`driver`),
   CONSTRAINT `FK5qsbxtn27vian263fx7awoayw` FOREIGN KEY (`driver`) REFERENCES `drivers` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `transactions`
 --
 
 DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transactions` (
   `type` varchar(6) NOT NULL,
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -159,13 +203,17 @@ CREATE TABLE `transactions` (
   KEY `FK6cip975i2cm6jheh4bdb391wn` (`stopover`),
   CONSTRAINT `FK6cip975i2cm6jheh4bdb391wn` FOREIGN KEY (`stopover`) REFERENCES `stopovers` (`id`),
   CONSTRAINT `FKfyxndk58yiq2vpn0yd4m09kbt` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `trucks`
 --
 
 DROP TABLE IF EXISTS `trucks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `trucks` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `bookedUntil` datetime(6) NOT NULL,
@@ -179,7 +227,18 @@ CREATE TABLE `trucks` (
   UNIQUE KEY `UK_k9gnwpt2w3e1m25ft4nvm6vsy` (`regNumber`),
   KEY `FKnra9j9k3wvnmtm87u03q2lyi4` (`location`),
   CONSTRAINT `FKnra9j9k3wvnmtm87u03q2lyi4` FOREIGN KEY (`location`) REFERENCES `cities` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
--- Dump completed on 2018-09-22 16:01:49
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-28 11:04:35
--- scripts/4-test-data.sql
+++ scripts/4-test-data.sql
@@ -1,33 +1,154 @@
 USE `logistika`;
 
+-- MySQL dump 10.13  Distrib 5.7.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: logistika
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+--
+-- Dumping data for table `cities`
+--
+
 LOCK TABLES `cities` WRITE;
-INSERT INTO `cities` VALUES (6,'Вологда'),(4,'Москва'),(1,'Санкт-Петербург'),(2,'Старая Ладога'),(3,'Тверь'),(8,'Тихвин'),(7,'Череповец'),(5,'Ярославль');
+/*!40000 ALTER TABLE `cities` DISABLE KEYS */;
+INSERT INTO `cities` VALUES (6,'Вологда'),
+							(4,'Москва'),
+							(1,'Санкт-Петербург'),
+                            (2,'Старая Ладога'),
+                            (3,'Тверь'),
+                            (8,'Тихвин'),
+                            (7,'Череповец'),
+                            (5,'Ярославль');
+/*!40000 ALTER TABLE `cities` ENABLE KEYS */;
 UNLOCK TABLES;
 
-LOCK TABLES `drivers` WRITE;
-INSERT INTO `drivers` (`bookedUntil`, `firstName`, `lastName`, `personalId`, `location`) VALUES ('2018-09-01 00:00', 'Адам', 'Козлевич', '000123', '1'), ('2018-09-01 00:00', 'Александр', 'Балаганов', '000345', '1'), ('2018-09-01 00:00', 'Михаил', 'Паниковский', '000013', '1');
-UNLOCK TABLES;
-
-LOCK TABLES `orders` WRITE;
-INSERT INTO `orders` VALUES (1,'2018-09-17 03:19:23.958000','Слон','NEW',1200,1,7),(2,'2018-09-17 03:19:24.316000','Бегемот','NEW',1000,2,6),(3,'2018-09-17 03:19:24.316000','Гусь','NEW',50,2,6),(4,'2018-09-17 03:19:24.316000','Поросенок','NEW',100,7,1);
-UNLOCK TABLES;
+--
+-- Dumping data for table `roads`
+--
 
 LOCK TABLES `roads` WRITE;
-INSERT INTO `roads` VALUES (1,134,2,1),(2,134,1,2),(3,533,3,1),(4,533,1,3),(5,176,4,3),(6,176,3,4),(7,328,5,3),(8,328,3,5),(9,272,5,4),(10,272,4,5),(11,196,6,5),(12,196,5,6),(13,136,7,6),(14,136,6,7),(15,319,8,7),(16,319,7,8),(17,105,8,2),(18,105,2,8);
+/*!40000 ALTER TABLE `roads` DISABLE KEYS */;
+INSERT INTO `roads` VALUES 	(1,134,2,1),(2,134,1,2),
+							(3,533,3,1),(4,533,1,3),
+                            (5,176,4,3),(6,176,3,4),
+                            (7,328,5,3),(8,328,3,5),
+                            (9,272,5,4),(10,272,4,5),
+                            (11,196,6,5),(12,196,5,6),
+                            (13,136,7,6),(14,136,6,7),
+                            (15,319,8,7),(16,319,7,8),
+                            (17,105,8,2),(18,105,2,8);
+/*!40000 ALTER TABLE `roads` ENABLE KEYS */;
 UNLOCK TABLES;
 
-LOCK TABLES `stopovers` WRITE;
-INSERT INTO `stopovers` VALUES (1,NULL,0,1200,1,1),(2,NULL,2,0,1,1),(3,NULL,1,100,7,1);
-UNLOCK TABLES;
-
-LOCK TABLES `tickets` WRITE;
-INSERT INTO `tickets` VALUES (1,NULL,13,'2018-09-25 21:00:00.000000','CREATED',1);
-UNLOCK TABLES;
-
-LOCK TABLES `transactions` WRITE;
-INSERT INTO `transactions` VALUES ('LOAD',1,1,1),('UNLOAD',2,1,3),('LOAD',3,4,3),('UNLOAD',4,4,2);
-UNLOCK TABLES;
+--
+-- Dumping data for table `trucks`
+--
 
 LOCK TABLES `trucks` WRITE;
-INSERT INTO `trucks` VALUES (1,'2018-09-17 03:17:54.103000',5000,1,1,'AA00000',1,1),(2,'2018-09-17 03:18:07.669000',20000,1,1,'AB12345',2,7),(3,'2018-09-17 03:18:22.727000',15000,1,0,'AC33333',2,5);
+/*!40000 ALTER TABLE `trucks` DISABLE KEYS */;
+INSERT INTO `trucks` VALUES (1,'2099-12-31 23:59:00.000000',5000,0,1,'AA00000',1,1),
+							(2,'2018-09-17 03:18:07.669000',20000,1,0,'AB12345',2,7),
+							(3,'2018-09-17 03:18:22.727000',5000,1,1,'AC33333',1,5),
+							(5,'2018-09-28 09:20:30.582000',20000,1,1,'ZZ99999',2,1);
+/*!40000 ALTER TABLE `trucks` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
+--
+-- Dumping data for table `drivers`
+--
+
+LOCK TABLES `drivers` WRITE;
+/*!40000 ALTER TABLE `drivers` DISABLE KEYS */;
+INSERT INTO `drivers` VALUES (1,'2018-09-01 00:00:00.000000','Адам','Козлевич','000123','akozlevich',1),
+							(2,'2018-09-26 20:00:00.000000','Александр','Балаганов','000345','abalaganov',1),
+                            (3,'2018-09-01 00:00:00.000000','Михаил','Паниковский','000013','mpanikovski',5);
+/*!40000 ALTER TABLE `drivers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,'2018-09-17 03:19:23.958000','Слон','DELIVERED',1200,1,7),
+							(2,'2018-09-17 03:19:24.316000','Бегемот','DELIVERED',1000,2,6),
+                            (3,'2018-09-17 03:19:24.316000','Сиг г/к','NEW',800,2,6),
+                            (4,'2018-09-17 03:19:24.316000','Лом цветных металлов','ASSIGNED',650,7,1),
+                            (5,'2018-09-27 15:08:13.359000','Шампунь финский','ASSIGNED',1000,1,7),
+                            (6,'2018-09-28 10:44:05.428000','Швеллер гнутый равнополочный','NEW',3500,7,1),
+                            (7,'2018-09-28 10:45:37.176000','Буссоли','NEW',1300,6,8),
+                            (8,'2018-09-28 11:02:50.161000','Масло сливочное 82.5%','NEW',3000,6,4);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Dumping data for table `tickets`
+--
+
+LOCK TABLES `tickets` WRITE;
+/*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
+INSERT INTO `tickets` VALUES (13,'2018-09-26 20:00:00.000000',17,4,'2018-09-25 09:00:00.000000','CLOSED',1),
+							(14,NULL,16,-1,'2018-09-29 09:00:00.000000','CREATED',1);
+/*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `stopovers`
+--
+
+LOCK TABLES `stopovers` WRITE;
+/*!40000 ALTER TABLE `stopovers` DISABLE KEYS */;
+INSERT INTO `stopovers` VALUES (1,7200000000000,0,1200,1,13),(2,46800000000000,4,0,1,13),(3,32400000000000,2,1000,7,13),(4,14400000000000,1,2200,2,13),(5,14400000000000,3,0,6,13),(6,NULL,0,1000,1,14),(7,NULL,2,0,1,14),(8,NULL,1,650,7,14);
+/*!40000 ALTER TABLE `stopovers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES ('LOAD',1,1,1),('UNLOAD',2,1,3),('LOAD',3,2,4),('UNLOAD',4,2,5),('LOAD',5,5,6),('UNLOAD',6,5,8),('LOAD',7,4,8),('UNLOAD',8,4,7);
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `shifts`
+--
+
+LOCK TABLES `shifts` WRITE;
+/*!40000 ALTER TABLE `shifts` DISABLE KEYS */;
+INSERT INTO `shifts` VALUES (13,2);
+/*!40000 ALTER TABLE `shifts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-28 11:04:35
