ccf5ab86b6e37932db7d842c66d24ebbf32f662c
MartaEN <marta.the.developer@gmail.com>
2018-10-03 21:59:11 +0300

    ExceptionHandlingControllerAdvice and most basic error reporting to user

--- src/main/java/com/marta/logistika/configuration/WebInitializer.java
+++ src/main/java/com/marta/logistika/configuration/WebInitializer.java
@@ -1,6 +1,8 @@
 package com.marta.logistika.configuration;
 
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.Filter;
@@ -20,6 +22,13 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected DispatcherServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+        final DispatcherServlet dispatcherServlet = (DispatcherServlet) super.createDispatcherServlet(servletAppContext);
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        return dispatcherServlet;
     }
 
     @Override
--- src/main/webapp/WEB-INF/views/common/error.jsp
+++ src/main/webapp/WEB-INF/views/common/error.jsp
@@ -1,16 +1,12 @@
+<jsp:useBean id="error" scope="request" type="com.marta.logistika.dto.ErrorMessage"/>
 <%@ page contentType="text/html;charset=UTF-8"%>
 <%@ page isErrorPage="true"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <jsp:include page="../_fragments/page-template-before-main.jsp"/>
 
 
-<h3 class="modal-header">Error Page</h3>
-URL: ${url}
-<br/>
-Exception: ${exception.message}
-<c:forEach items="${exception.stackTrace}" var="exceptionStackTrace">
-    ${exceptionStackTrace}
-</c:forEach>
-
+<h3 class="modal-header"><spring:message code='${error.title}'/></h3>
+<spring:message code='${error.message}'/>
 
 <jsp:include page="../_fragments/page-template-after-main.jsp"/>
