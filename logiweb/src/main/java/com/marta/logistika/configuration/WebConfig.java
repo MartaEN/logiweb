ef2331579711497ec0d3c99152091eb5c8d78c1e
MartaEN <marta.the.developer@gmail.com>
2018-10-23 00:20:26 +0300

    Controller testing support

--- logiweb/src/main/java/com/marta/logistika/configuration/WebConfig.java
+++ logiweb/src/main/java/com/marta/logistika/configuration/WebConfig.java
@@ -33,11 +33,9 @@ public class WebConfig implements WebMvcConfigurer {
 	
 	@Bean
 	public InternalResourceViewResolver setupViewResolver(){
-		
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
-
 		return resolver;
 	}
 
